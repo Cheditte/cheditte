1cab29fe7afd9042c66aa1696b456f8570dd16ee
Cheditte <s.ali.emre.altuntas@gmail.com>
2021-02-02 14:59:54 +0300

    Prettier Edit

--- pages/cheditte/index.js
+++ pages/cheditte/index.js
@@ -1,19 +1,21 @@
-import Link from 'next/link';
-import Head from 'next/head';
+import Link from 'next/link'
+import Head from 'next/head'
 
 const cheditte = () => {
-    return (
-        <>
-            <Head>
-                <title>Cheditte | Cheditte List</title>
-                <meta name="keywords" content="cheditte" />
-            </Head>
-            <div>
-                <h1>Cheditte List</h1>
-                <Link href="/"><a>Back to Homepage</a></Link>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Cheditte | Cheditte List</title>
+        <meta name="keywords" content="cheditte" />
+      </Head>
+      <div>
+        <h1>Cheditte List</h1>
+        <Link href="/">
+          <a>Back to Homepage</a>
+        </Link>
+      </div>
+    </>
+  )
 }
 
-export default cheditte;
+export default cheditte
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import styles from '../styles/Home.module.css';
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
 
- const Home = () => {
+const Home = () => {
   return (
     <>
       <Head>
@@ -12,26 +12,32 @@ import styles from '../styles/Home.module.css';
       <div className={styles.container}>
         <div className={styles.title}>HomePage</div>
         <p className={styles.text}>
-          Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir.
-          Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak
-          üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı
-          sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış,
-          aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum
-          pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker
-          gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
-          </p>
+          Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır
+          metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat
+          numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı
+          1500'lerden beri endüstri standardı sahte metinler olarak
+          kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış,
+          aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır.
+          1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının
+          yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum
+          sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
+        </p>
         <p className={styles.text}>
-          Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir.
-          Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak
-          üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı
-          sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış,
-          aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum
-          pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker
-          gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
-          </p>
-          <Link href="/cheditte"><a className={styles.btn}>See Cheditte List</a></Link>
+          Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır
+          metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat
+          numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı
+          1500'lerden beri endüstri standardı sahte metinler olarak
+          kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış,
+          aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır.
+          1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının
+          yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum
+          sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
+        </p>
+        <Link href="/cheditte">
+          <a className={styles.btn}>See Cheditte List</a>
+        </Link>
       </div>
     </>
   )
 }
-export default Home;
+export default Home
